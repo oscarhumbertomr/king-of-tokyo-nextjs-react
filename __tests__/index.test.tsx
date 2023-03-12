5d6b99c0deb1eeb42f2a9e7b2ad4dec9337efa40
Oscar Montejo <oscarhumbertomr@gmail.com>
2023-03-12 17:31:26 -0600

    hosky and unit tests added

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/pages/index'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    // const heading = screen.getByRole('heading', {
+    //   name: /King of Tokyo Dashboard\.js!/i,
+    // })
+
+    const header = screen.queryAllByText('King of Tokyo Dashboard')
+
+    expect(header).toBeTruthy()
+  })
+})
