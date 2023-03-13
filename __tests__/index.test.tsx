18adeea0df6134423b6f9decb16233eec56fa648
Oscar Montejo <oscarhumbertomr@gmail.com>
2023-03-12 18:13:11 -0600

    fix unit tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -4,13 +4,7 @@ import Home from '@/pages/index'
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
-
-    // const heading = screen.getByRole('heading', {
-    //   name: /King of Tokyo Dashboard\.js!/i,
-    // })
-
-    const header = screen.queryAllByText('King of Tokyo Dashboard s')
-
+    const header = screen.getByText('King of Tokyo Dashboard')
     expect(header).toBeTruthy()
   })
 })
