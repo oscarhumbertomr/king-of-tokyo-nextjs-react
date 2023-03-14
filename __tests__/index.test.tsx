c287ccb52df6aed1e0771ec58e5c58474f50f082
Oscar Montejo <oscarhumbertomr@gmail.com>
2023-03-14 01:41:16 -0600

    eslint config and lint-staged with husky

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -5,7 +5,6 @@ describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />);
     const header = screen.getByText('King of Tokyo Dashboard');
-
     expect(header).toBeTruthy();
   });
 });
